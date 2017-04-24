9e310c1c3343511c5dacf1134917c65a272a24bd
Andrei Ciobanu <Andrei Ciobanu>
2017-04-24 13:29:44 +0300

    added progressBar

---
+++ myPad/src/notepad/ProgressBarColor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notepad;
+
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+
+/**
+ *
+ * @author andre
+ */
+public class ProgressBarColor {
+  
+    public ProgressBarColor() {
+        
+        
+         UIManager.put("nimbusOrange",new ColorUIResource(new Color(51,153,255)));
+        
+         
+     
+    }
+}
